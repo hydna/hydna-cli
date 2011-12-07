57af071c4763c298d72edb86db74870e350111bc
Gustaf Sjöberg <gs@distrop.com>
2011-12-07 18:07:28 +0100

    Added distutils script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='hydna', version='0.1',
+      description='Hydna command-line interface.',
+      author='Gustaf Sjöberg', author_email='gs@hydna.com',
+      scripts=['hydna'])
